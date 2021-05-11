d9c5217008fcb6838b9050e200cded4df322c049
samuelayo <ayoogundipe2005@gmail.com>
2021-05-11 10:34:35 +0100

    add login and signup query

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
+const authController = require('../controller/auth')
 
 /**
  * This function acts as the health check route.
@@ -15,10 +16,15 @@ router.get('/', (req, res) => {
  * This function enables the user to log in to the system
  * @param req {object}. An express request object, which contains body, 
  * an object containing the username, password
- * @return npm {Object}. user data
+ * @return npm {Object}. { ok: boolean -> status of request, data: object -> user data, message: string -> usually present on errors, returning error message}
  */
-router.post('/user/login', (req, res) => {
-
+router.post('/user/login', async (req, res) => {
+    try {
+        const data = await authController.login(req);
+        res.status(200).json({ ok: true, data })
+    } catch (error) {
+        res.status(400).json({ ok: false,  message: `Error occured while processing request with: ${ error && error.message }`})
+    }
 })
 
 
